46c942b80488ca10669b9b00419b89581efa4eb3
Solonarv <nstamm@gmx.de>
2015-10-19 12:31:08 +0200

    Some work on world update code

--- World.hs
+++ World.hs
@@ -3,6 +3,7 @@
 module World where
 
 import GHC.Float
+import Data.List
 
 import Linear
 
@@ -13,7 +14,7 @@ data MovingPosition = MovingPosition {
     position :: V2 Double,
     velocity :: V2 Double,
     mass :: Double
-}
+} deriving Show, Eq
 
 stepPosition :: Double -> MovingPosition -> MovingPosition
 stepPosition dt mpos = mpos { position = dt *^ v ^+^ r }
@@ -26,7 +27,7 @@ class HasMovingPosition a where
     updatePosition :: a -> (MovingPosition -> MovingPosition) -> a
     updatePosition x f = setPosition x $ f $ movingPosition x
 
-data Rotation = Clockwise | Counterclockwise | NoRotation
+data Rotation = Clockwise | Counterclockwise | NoRotation deriving Show, Eq
 rotationToFactor :: Num a => Rotation -> a
 rotationToFactor Clockwise = -1
 rotationToFactor Counterclockwise = 1
@@ -36,8 +37,9 @@ data Ship = Ship {
     facing :: Double, -- in radians, not degrees
     rotateDirection :: Rotation,
     thrusting :: Bool,
+    health :: Double
     movingPosition'Ship :: MovingPosition
-}
+} deriving Show, Eq
 
 rotateShip :: Double -> Ship -> Ship
 rotateShip dt ship = ship { facing = facing ship + dt * Constants.rotationRate * rotationToFactor (rotateDirection ship) }
@@ -52,9 +54,9 @@ instance HasMovingPosition Ship where
     setPosition s p = s { movingPosition'Ship = p }
 
 data Asteroid = Asteroid {
-    size :: Double,
+    size :: Double, -- radius
     movingPosition'Asteroid :: MovingPosition
-}
+} deriving Show, Eq
 
 instance HasMovingPosition Asteroid where
     movingPosition = movingPosition'Asteroid
@@ -66,10 +68,26 @@ data World = World {
     ship :: Ship,
     projectiles :: [Projectile],
     asteroids :: [Asteroid]
-}
+} deriving Show, Eq
 
 stepWorld :: Float -> World -> World
 stepWorld dt' w = let dt = float2Double dt' in
-    World {
+    checkCollisions $ w {
         ship = flip updatePosition (stepPosition dt) $ accelerateShip dt $ rotateShip dt $ ship w
+        projectiles = map (stepPosition dt) $ projectiles w
+        asteroids = map (updatePosition $ stepPosition dt) $ asteroids w
     }
+
+checkCollisions :: World -> World
+checkCollisions w = let
+    asteroidCollisions = nubBy (\(a1, a2) (a3, a4) ->
+        a1 == a3 || a2 == a3 || a1 == a4 || a2 == a2)
+        $ filter (\(a1, a2) ->
+            let r1 = position $ movingPosition a1
+                r2 = position $ movingPosition a2
+                dr = r1 ^-^ r2
+                dmax = size a1 + size a2
+            in a1 /= a2 && dmax * dmax <= dr ^*^ dr) $ asteroids w
+    breakingAsteroids1 = -- TODO figure out how to calculate collisions
+    in
+        w -- TODO make actual changes to the world
