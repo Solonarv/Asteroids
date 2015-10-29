d338f484b4004b647cac993e3f9ac4ca19f24722
Solonarv <nstamm@gmx.de>
2015-10-29 16:38:57 +0100

    Ship should render now

--- Render.hs
+++ Render.hs
@@ -12,13 +12,21 @@ class Renderable a where
 
 instance Renderable Object where
     render o = translateBy (position $ movingPosition o) $ case typ o of
-        Asteroid -> circleSolid (size o)
-        Bullet -> circleSolid 1
+        Asteroid -> Color brown $ circleSolid (size o)
+        Bullet -> Color white $ circleSolid 1
 
 instance Renderable Ship where
     render s = translateBy (position $ movingPosition o) $
                 liftF Rotate (facing s) $
                 Pictures [
+                    Color white $ Polygon [(0,10), (6,-4), (0,-2), (-6,-4)],
+                    if thrusting s
+                        then Color orange $ Polygon [(-3,-3), (0,-2), (3,-3), (0,-5)]
+                        else Blank, 
+                    Blank {- Color gray $ case rotateDirection s of
+                        NoRotation -> Blank
+                        Clockwise -> Pictures [Arc 95 130 10, Arc 275 300 5]
+                        Counterclockwise -> Pictures [Arc 50 85 10, Arc -120 -95 5] {- -}
                     
                 ]
     
